5a32a0e80f512be062025188da2a37a36e645183
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2026-02-19 22:54:50 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vite.dev/config/
-// Config updated
-export default defineConfig({
-    plugins: [vue()],
-    base: '/vendor/',
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [vue()],
+        base: env.VITE_BASE_URL || '/',
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
-    server: {
-        port: 5002,
-        host: true,
-        strictPort: true,
-        allowedHosts: ['consecratory-circinately-shad.ngrok-free.dev'],
-        proxy: {
-            '/vendor/api': {
-                target: 'http://localhost:8082',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/vendor/, '')
+        server: {
+            port: 5002,
+            host: true,
+            strictPort: true,
+            allowedHosts: 'all',
+            proxy: {
+                '/vendor/api': {
+                    target: 'http://localhost:8082',
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/vendor/, '')
+                }
             }
         }
     }
 })
+
